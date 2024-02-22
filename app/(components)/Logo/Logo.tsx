70bb022dfab78384ee6115630032de28f2a252ed
dyoneNero <dyonenero@gmail.com>
2024-02-22 18:39:02 +0300

    fg

--- app/(components)/Logo/Logo.tsx
+++ app/(components)/Logo/Logo.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import LogoImg from "@/public/logo_drevo.svg";
 
 const Logo = () => {
-  return <Image src={LogoImg} width="200" height="171" alt="-" />;
+  return <Image src="/logo_drevo.svg" width="200" height="171" alt="-" />;
 };
 
 export default Logo;
