0d59520ad77ec105ba9cb94b2c86ce0798e41e6b
dyoneNero <dyonenero@gmail.com>
2024-02-23 11:23:52 +0300

    yandex-webmaster

--- app/(components)/Logo/Logo.tsx
+++ app/(components)/Logo/Logo.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return <Image src="/logo_drevo.svg" width="200" height="171" alt="-" />;
+  return (
+    <Image
+      src="/logo_drevo.svg"
+      width="200"
+      height="171"
+      alt="-"
+      priority={true}
+    />
+  );
 };
 
 export default Logo;
