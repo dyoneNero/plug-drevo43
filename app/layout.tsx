50b8b35a8e0df84df5ecfdccc6a1ecb17d8270f3
dyoneNero <dyonenero@gmail.com>
2024-02-26 11:31:28 +0300

    yandex-metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <Head>
-        <Script id="metrika-counter" strategy="afterInteractive">
-          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      <body className={inter.className}>{children}</body>
+      <Script id="metrika-counter" strategy="afterInteractive">
+        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -33,9 +33,7 @@ export default function RootLayout({
                 accurateTrackBounce:true,
                 webvisor:true
           });`}
-        </Script>
-      </Head>
-      <body className={inter.className}>{children}</body>
+      </Script>
     </html>
   );
 }
