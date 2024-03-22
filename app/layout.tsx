36f2cb27f0143dd0c717c21b290391f930f399ca
dyoneNero <dyonenero@gmail.com>
2024-03-22 09:12:40 +0300

    update ym

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
-import Image from "next/image";
+import { Suspense } from "react";
+import { Metrika } from "./(components)/Metrika/Metrika";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "drevo43",
-  description: "Мы занимаемся строительством деревянных домов, беседок, бань",
+  title: "Строительство деревянных домов, бань и беседок",
+  description:
+    "Профессиональная команда строителей, которой уверенно можно  доверить самую ответственную работу - строительство дома. Наш опыт в этой сфере - более 12 лет, поэтому мы можем гарантировать качество работы.",
 };
 
 export default function RootLayout({
@@ -19,30 +20,9 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={inter.className}>{children}</body>
-      <Script id="metrika-counter" strategy="afterInteractive">
-        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-          ym(96573330, "init", {
-                defer: true,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-          });`}
-      </Script>
-      <noscript>
-        <div>
-          <Image
-            src="https://mc.yandex.ru/watch/96573330"
-            className="absolute -left-96"
-            alt=""
-          />
-        </div>
-      </noscript>
+      <Suspense>
+        <Metrika />
+      </Suspense>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import Container from "./(components)/Container/Container";
 import BlockInfo from "./(components)/BlockInfo/BlockInfo";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="bg-gray h-screen flex justify-center">
-      <Head>
-        <meta name="yandex-verification" content="f44a600ceff299c8" />
-      </Head>
       <Container>
         <BlockInfo />
       </Container>
