6b8e808cd8f64f9cb7547ef559334c729ed4f6b1
dyoneNero <dyonenero@gmail.com>
2024-03-22 09:20:02 +0300

    update ym

--- app/(components)/Metrika/Metrika.tsx
+++ app/(components)/Metrika/Metrika.tsx
@@ -6,17 +6,16 @@ export function Metrika() {
     <Script id="yandex-metrika">
       {`
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(96573330, "init", {
-          defer: true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true
-        });    
+   ym(96819926, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });    
       `}
     </Script>
   );
